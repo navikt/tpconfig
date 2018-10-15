a98e6bcb8a2b44a09fb13362294ee473224c1600
Marius Eriksen <Marius.Eriksen@nav.no>
2018-10-15 15:31:00 +0200

    Metrics from the jvm like memory usage etc. are available on the metrics endpoint

--- src/main/java/no/nav/tpconfig/Application.java
+++ src/main/java/no/nav/tpconfig/Application.java
@@ -5,6 +5,7 @@ import no.nav.tpconfig.server.Server;
 import no.nav.tpconfig.server.ServiceAccount;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import io.prometheus.client.hotspot.DefaultExports;
 
 import javax.servlet.ServletException;
 import java.net.InetAddress;
@@ -22,6 +23,7 @@ class Application {
 
     void run() {
         try {
+            initializeJvmMetrics();
             final String hostName = InetAddress.getLocalHost().getHostName();
             final TpConfig tpConfig = new TpConfig();
             final ServiceAccount serviceAccount = new ServiceAccount(tpConfig);
@@ -35,5 +37,9 @@ class Application {
             LOG.error("Could not start metrics servlet.", e);
             System.exit(1);
         }
+    }
+
+    private void initializeJvmMetrics() {
+        DefaultExports.initialize();
     }
 }
