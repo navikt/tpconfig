794e7d7d8a640682caf55da2e296720932082503
Marius Eriksen <marius.eriksen@nav.no>
2019-05-16 09:34:39 +0200

    Fixed indentation, removed unused constructor and added some newlines for readability

--- src/main/java/no/nav/tpconfig/server/EndpointFactory.java
+++ src/main/java/no/nav/tpconfig/server/EndpointFactory.java
@@ -14,10 +14,10 @@ public class EndpointFactory {
 
 
     private final static Metrics METRICS_FOR_SERVICEACCOUNT_ENDPOINT = metricFor("tp_config_serviceaccount")
-                .withFoundCounter("Antall serviceaccounts funnet basert på tpnr i request")
-                .withNotFoundCounter("Antall serviceaccounts ikke funnet basert på tpnr i request")
-                .withReceivedCounter("Antall requests mottatt til serviceaccount endepunkt")
-                .createMetrics();
+            .withFoundCounter("Antall serviceaccounts funnet basert på tpnr i request")
+            .withNotFoundCounter("Antall serviceaccounts ikke funnet basert på tpnr i request")
+            .withReceivedCounter("Antall requests mottatt til serviceaccount endepunkt")
+            .createMetrics();
     private static final Metrics METRICS_FOR_TPNR_TO_TPLEVERANDOER = metricFor("tp_config_tpleverandoer_tpnr")
             .withFoundCounter("Antall tpleverandoerer funnet basert på tpnr i request")
             .withNotFoundCounter("Antall serviceaccounts ikke funnet basert på tpnr i request")
@@ -29,19 +29,31 @@ public class EndpointFactory {
             .withReceivedCounter("Antall requests mottatt til tpleverandoer for tssnr endepunkt")
             .createMetrics();
 
-    private EndpointFactory() {
-    }
-
     public static Endpoint<String> createServiceAccountEndpoint(TpConfig tpConfig){
-        return new Endpoint<>(METRICS_FOR_SERVICEACCOUNT_ENDPOINT, Utlis.urlParamExtractor(TPNR_URL_PARAMETER_NAME), tpConfig::serviceaccount, SERVICE_ACCOUNT_PATH);
+        return new Endpoint<>(
+                METRICS_FOR_SERVICEACCOUNT_ENDPOINT,
+                Utlis.urlParamExtractor(TPNR_URL_PARAMETER_NAME),
+                tpConfig::serviceaccount,
+                SERVICE_ACCOUNT_PATH
+        );
     }
 
     public static Endpoint<String> createTpNrToTPLeverandoerEndpoint(TpConfig tpConfig){
-        return new Endpoint<>(METRICS_FOR_TPNR_TO_TPLEVERANDOER, Utlis.urlParamExtractor(TPNR_URL_PARAMETER_NAME), tpConfig::leverandoerNameByTPNr, LEVERANDOR_BY_TPNR_PATH);
+        return new Endpoint<>(
+                METRICS_FOR_TPNR_TO_TPLEVERANDOER,
+                Utlis.urlParamExtractor(TPNR_URL_PARAMETER_NAME),
+                tpConfig::leverandoerNameByTPNr,
+                LEVERANDOR_BY_TPNR_PATH
+        );
     }
 
     public static Endpoint<String> createTssNrToTPLeverandoerEndpoint(TpConfig tpConfig){
-        return new Endpoint<>(METRICS_FOR_TSSNR_TO_TPLEVERANDOER, Utlis.urlParamExtractor(TSSNR_URL_PARAMETER_NAME), tpConfig::leverandoerNameByTSSNr, LEVERANDOR_BY_TSSNR_PATH);
+        return new Endpoint<>(
+                METRICS_FOR_TSSNR_TO_TPLEVERANDOER,
+                Utlis.urlParamExtractor(TSSNR_URL_PARAMETER_NAME),
+                tpConfig::leverandoerNameByTSSNr,
+                LEVERANDOR_BY_TSSNR_PATH
+        );
     }
 
 }
